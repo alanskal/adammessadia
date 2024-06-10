698a39b7146cd78a9193edd8346003a3429b2da9
SKALSKI Alan <AlanSkalski@outlook.fr>
2024-06-10 23:53:04 +0200

    randomize position des pop up

--- app/ui/adult-ad.tsx
+++ app/ui/adult-ad.tsx
@@ -3,20 +3,18 @@
 // components/AdultAd.js
 
 import { useState, useEffect } from 'react';
-import Draggable, {DraggableCore} from "react-draggable";
+import Draggable from "react-draggable";
 
 interface AdultAdProps {
   content: string;
   id: number;
   timeOut: number;
-
 }
 
-const AdultAd: React.FC<AdultAdProps> = ({content, id, timeOut}) => {
+const AdultAd: React.FC<AdultAdProps> = ({ content, id, timeOut }) => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-
     const timer = setTimeout(() => {
       setIsVisible(true);
     }, timeOut);
@@ -25,32 +23,42 @@ const AdultAd: React.FC<AdultAdProps> = ({content, id, timeOut}) => {
     return () => clearTimeout(timer);
   }, [timeOut]);
 
+  useEffect(() => {
+    if (isVisible) {
+      RandomPosition();
+    }
+  }, [isVisible]);
+
   const handleClose = () => {
     setIsVisible(false);
   };
 
+  const RandomPosition = () => {
+    const randomTop = Math.random() * 500 + 'px';
+    const randomLeft = Math.random() * 600 + 'px';
+    const randomPosition = { top: randomTop, left: randomLeft };
+    return randomPosition;
+  };
+
   return (
     <>
-
       {isVisible && (
-      <Draggable>
-        <div className={`mac-popup-${id}`}>
-          <div className="mac-popup-header">
-            <div className="mac-popup-buttons">
-              <div className="mac-popup-button close" onClick={handleClose}></div>
-              <div className="mac-popup-button minimize"></div>
-              <div className="mac-popup-button zoom"></div>
+        <Draggable>
+          <div className={`mac-popup-${id}`} style={RandomPosition()}>
+            <div className="mac-popup-header">
+              <div className="mac-popup-buttons">
+                <div className="mac-popup-button close" onClick={handleClose}></div>
+                <div className="mac-popup-button minimize"></div>
+                <div className="mac-popup-button zoom"></div>
+              </div>
+              <div className="mac-popup-title">ADAM MESSAADIA</div>
             </div>
-            <div className="mac-popup-title">ADAM MESSAADIA</div>
-          </div>
-          <div className="mac-popup-content">
-            <p>{content}</p>
-            <div>
-
+            <div className="mac-popup-content">
+              <p>{content}</p>
+              <div></div>
             </div>
           </div>
-        </div>
-      </Draggable>
+        </Draggable>
       )}
     </>
   );
