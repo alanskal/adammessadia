8153b37da6197b86dab3b48e5cde55fd350e7751
SKALSKI Alan <AlanSkalski@outlook.fr>
2024-07-17 17:54:22 +0200

    c la bonne i guess

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,22 +22,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Head>
       <title>Adam Messaadia</title>
     </Head>
-
-    <CartProvider
-     mode="payment"
-     cartMode="client-only"
-     stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}
-     successUrl={`${process.env.NEXT_PUBLIC_URL}/success`}
-     cancelUrl={`${process.env.NEXT_PUBLIC_URL}/?success=false`}
-     currency="EUR"
-     allowedCountries={["FR"]}
-     shouldPersist={true}
-   >
       <div className='bg'>
         <NavbarAlt />
           <Component {...pageProps} />;
       </div>
-    </CartProvider>
   </>
 
   )
